9fc86ee06bd2b543824b36449986048a6b9205b8
Kevin Larsson <lakevin@student.chalmers.se>
2016-11-18 12:23:02 +0100

    Added system for seat numbering and assigning using Python dictionaries. Also displays how you can iterate through lists of objects. Adds a simple method for moving forward in the aisle.

--- airplane.py
+++ airplane.py
@@ -1,18 +1,35 @@
 import numpy as np
 
 class Passenger():
+    nPassenger = 0  # Class variable keeping track of the number of passengers in total
     def __init__(self, seat, speed):
-        self.seat = seat
+        self.seat = seat    # Of form {'side':'L', 'row':0, 'number': 0}. row 0 is by entrance, number 0 is by aisle
         self.speed = speed
+        self.i = self.nPassenger    # Assigning unique index for every passenger
+        self.nPassenger += 1
 
 class Airplane():
-    def __init__(self, nRows, nSeatsPerSide, nPassengers):
+    def __init__(self, nRows, nSeatsPerSide):
         self.nRows = nRows
         self.nSeatsPerRow = nSeatsPerSide
-        self.waitingList = [ Passenger('3', 0.5) for iPassenger in range(nPassengers)]
-        self.aisle = ['e' for iRows in range(nRows)]
-        self.leftHandSeats = [[ 'e' for iSeats in range(nSeatsPerSide)] for iRows in range(nRows)]
-        self.rightHandSeats = [['e' for iSeats in range(nSeatsPerSide)] for iRows in range(nRows)]
 
-airplane = Airplane(5, 2, 10)
-print airplane.waitingList
+        # Create a list of all seats available in the flight
+        seatList = [{'side':'L', 'row':iRow, 'number':iNumber} for iRow, iNumber in np.ndindex((nRows,nSeatsPerSide))]
+
+        # Creating a Passenger for each seat and assigns the seat for them. Passengers to be kept in this list
+        self.passengers = [ Passenger(iSeat, 0.5) for iSeat in seatList ]
+        # Copies list of passengers into waiting list. Same objects kept in both list (elements fo lists works as pointers)
+        self.waitingList = list(self.passengers)
+
+        self.aisle = ['' for iRows in range(nRows)]
+        self.leftHandSeats = [[ '' for iSeats in range(nSeatsPerSide)] for iRows in range(nRows)]
+        self.rightHandSeats = [['' for iSeats in range(nSeatsPerSide)] for iRows in range(nRows)]
+
+    def moveForward(self, aislePosition):
+        # Moves the passenger in aislePosition one step forward if next position is empty
+        if self.aisle[aislePosition + 1] == '':
+            self.aisle[aislePosition + 1] = self.aisle[aislePosition]
+            self.aisle[aislePosition] = ''
+
+
+airplane = Airplane(5, 2)
