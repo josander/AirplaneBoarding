2337deb94227910e9b0ff45e9c0aad9586c605e5
jossan_cuellar <josander@student.chalmers.se>
2016-11-23 11:48:55 +0100

    Main file created

---
+++ main.py
@@ -0,0 +1,8 @@
+import airplane
+
+
+# TODO: Get data with statistical significance
+
+# TODO: Iterate for different sizes of the airplane
+airplane = airplane.Airplane(20,3,'flyingCarpet')
+airplane.board()
