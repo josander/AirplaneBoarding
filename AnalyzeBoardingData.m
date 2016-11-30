2a3cfbcf8ae810f19732868a9b32eae8d4a0944f
jossan_cuellar <josander@student.chalmers.se>
2016-11-30 15:56:36 +0100

    Matlab script to analyze the results added

---
+++ AnalyzeBoardingData.m
@@ -0,0 +1,31 @@
+%% Analyze boarding data
+
+data = dlmread('boardingData.txt');
+
+planeDim = data(1:5:end,:);
+nbrRows = planeDim(1:end/2,1);
+
+randomBoarding = data(2:5:end,:);
+backToFrontBoarding = data(3:5:end,:);
+outsideInBoarding = data(4:5:end,:);
+flyingCarpetBoarding = data(5:5:end,:);
+
+figure(1)
+plot(nbrRows,randomBoarding(1:end/2,1))
+hold on
+plot(nbrRows,backToFrontBoarding(1:end/2,1))
+plot(nbrRows,outsideInBoarding(1:end/2,1))
+plot(nbrRows,flyingCarpetBoarding(1:end/2,1))
+legend('Random','Back to front','Outside in','Flying carpet');
+ylabel('Time')
+xlabel('Rows')
+
+figure(2)
+plot(nbrRows,randomBoarding(end/2+1:end,1))
+hold on
+plot(nbrRows,backToFrontBoarding(end/2+1:end,1))
+plot(nbrRows,outsideInBoarding(end/2+1:end,1))
+plot(nbrRows,flyingCarpetBoarding(end/2+1:end,1))
+legend('Random','Back to front','Outside in','Flying carpet');
+ylabel('Time')
+xlabel('Rows')
